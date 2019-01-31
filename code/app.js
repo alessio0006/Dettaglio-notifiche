38ef574131f8981bc5d1a2c9026b60691f537717
Your Name <you@example.com>
2019-01-31 12:31:14 +0100

    gigi

--- code/app.js
+++ code/app.js
@@ -1,1 +1,18 @@
 alert("hai molte notifiche da leggere");
+app = {
+    init : function () { //init diventa n metodo della classe anonima di cui la variabile app fa parte. In questo modo app diventa l'unica variabile globale
+        console.log("init!"); //messaggio presente nel debug
+        $("title").text("Dettaglio Notifiche"); // ha preso il tag title e ho cambiato titolo
+        $.get("data.json").done(app.onSuccess).fail(app.onError);
+      
+    },
+    onSuccess:function (jsonData) {
+        console.log(jsonData);
+     
+        },
+        onError:function(e){
+            console.log("error!");
+            console.log(JSON.stringify(e));
+        }
+};
+$(document).ready(app.init);// implemento la funzione init presente nella classe di app
