5916e5a0da66b3af68e998c844ae412636ecc9d6
Your Name <you@example.com>
2019-05-30 12:31:40 +0200

    pagina dinamica

--- code/app.js
+++ code/app.js
@@ -1,14 +1,17 @@
-galert("hai molte notifiche da leggere");
+alert("hai molte notifiche da leggere");
 app = {
     init : function () { //init diventa n metodo della classe anonima di cui la variabile app fa parte. In questo modo app diventa l'unica variabile globale
         console.log("init!"); //messaggio presente nel debug
         $("title").text("Dettaglio Notifiche"); // ha preso il tag title e ho cambiato titolo
-        $.get("data.json").done(app.onSuccess).fail(app.onError);
+        $.get("data.json")
+        .done(app.onSuccess)
+        .fail(app.onError);
       
     },
+
     onSuccess:function (jsonData) {
         console.log(jsonData);
-        let lista=dataJson.notificationlist;
+        let lista=jsonData.notificationlist;
 		lista.forEach(element => {let list=`
 		<li>
 	        ${element.messaggio}<br>${element.timestamp}
